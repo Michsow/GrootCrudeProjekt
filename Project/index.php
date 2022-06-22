c9f2b877d17b3bec80bc339e8093ee3cf6543f78
Michsow <94047110+Michsow@users.noreply.github.com>
2022-06-22 15:54:31 +0200

    kurwa zmiany

--- Project/index.php
+++ Project/index.php
@@ -2,38 +2,33 @@
 
     <?php include "PHPfiles/connection.php" ?>     
     <?php include "PHPfiles/HeadFoot/header.php" ?>
-    <?php
-                              $data = $conn->query("SELECT * FROM infolocaties")->fetchAll();
-
-                              foreach ($data as $row)
-    ?>
     <main>
         <div class="break">           
 
         </div>
             <div class="images">
             <div class="slideshow-container">
-                
+              
+    <?php
+                              $data = $conn->query("SELECT * FROM infolocaties")->fetchAll();
+
+                              foreach ($data as $row) {
+
+    ?>
                 <div class="mySlides fade">
-                  <img src="img/AmerykaNewYork.jpg" style="width:100%">
+                  <img src="img/<?php echo $row ['Photo'];?>" style="width:100%">
                   <div>€<?php echo $row ['Price'];?></div>
                   <a href="PHPfiles/MoreInfoFlight.php?p=<?php echo $row ['LocationID'];?>" >geïnteresseerd? kijk hier!</a>
                 </div>
-                
-                <div class="mySlides fade">
-                  <img src="img/USALasVegasNight.jpg" style="width:100%">
-                  <div>€<?php echo $row ['Price'];?></div>
-                  <a href="PHPfiles/MoreInfoFlight.php?p=<?php echo $row ['LocationID'];?>">geïnteresseerd? kijk hier!</a>
-                </div>
-                
-                <div class="mySlides fade">
-                  <img src="img/ArgentinaBuenosAires.jpg" style="width:100%">
-                  <div>€ <?php echo $row ['Price'];?></div>
-                  <a href="PHPfiles/MoreInfoFlight.php?p=<?php echo $row ['LocationID'];?>" >geïnteresseerd? kijk hier!</a>
-                </div>
+    <?php 
+                              }
+    ?>
+              
                 </div>
                 <br>
                 <div style="text-align:center">
+                  <span class="dot"></span> 
+                  <span class="dot"></span> 
                   <span class="dot"></span> 
                   <span class="dot"></span> 
                   <span class="dot"></span> 
