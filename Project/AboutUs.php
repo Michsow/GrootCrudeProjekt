d7b534c31b0894eaf40ac3dcc0cfbb6f5a3f383b
KanashiQba <94380630+KanashiQba@users.noreply.github.com>
2022-06-28 20:59:04 +0200

    about us

--- Project/AboutUs.php
+++ Project/AboutUs.php
@@ -2,6 +2,26 @@
 
 <?php include "PHPfiles/HeadFoot/header.php" ?>
 <main>
+    <div class = "break">
+    </div>
+    <div class="break2">
+    </div>
+    <div class = "main-aboutus">
+        <div = class = "bedrijf-foto">
 
+        </div>
+        <div class = "info-1">
+            <h1>TriTaco is sinds 1977 ReisXpert in Best en omgeving.</h>
+            <br>
+            <br>
+            <h2>TriTaco is begonnen in de Hoofdstraat en vervolgens verhuisd naar de Boterhoek 83. TriTaco is sinds 1977 ReisXpert in Best en omgeving.</h2>
+            <br>
+            <a>Persoonlijke aandacht, service, kwaliteit en garantie voor de juiste prijs staan hierbij hoog in het vaandel. Dat vind je ook terug op onze website.
+            Jouw reis kunnen wij via onze vele zoekmachines in combinatie met onze eigen ervaringen, geheel op maat voor jou samenstellen. Desgewenst kun je ook een afspraak maken. Wij nemen je veel werk uit handen: reizen zonder zelf te hoeven zoeken! Ook voor het aanbod van de Duitse en Belgische reisorganisaties ben je bij ons aan het goede adres. De mooiste ervaringen, jouw fijnste herinneringen blijven tenslotte mensenwerk! Tevreden vakantiegangers zijn ons visitekaartje! </a>
+        </div>
+        <div class = "bedrijf-foto2">
+
+        </div>
+    </div>
 </main>
 <?php include "PHPfiles/HeadFoot/footer.php" ?>
