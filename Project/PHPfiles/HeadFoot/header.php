97febe098ab1c5c544eb6178f0fd7d7261fdbd36
Michsow <94047110+Michsow@users.noreply.github.com>
2022-06-29 13:52:22 +0200

    2

--- Project/PHPfiles/HeadFoot/header.php
+++ Project/PHPfiles/HeadFoot/header.php
@@ -40,7 +40,7 @@
             <a href="PHPfiles/Mijnaccount.php"> <button> Mijn account </button> </a>
         <div class="shoppingCartDiv">
             
-        <i class="fa fa-plane" style="font-size:68px;" aria-hidden="true"></i>
+        <i class="fa-solid fa-plane-departure" style="font-size:68px;" aria-hidden="true"></i>
         <div class="break4"></div>
         <button class="shoppingCartButton" style="width:100px;"><a href="http://localhost/ProjectCrudeGroot/Project/GrootCrudeProjekt/Project/PHPfiles/bookedFlights.php">Booked flights</a></button>
         </div>
