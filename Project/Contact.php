e9ddbe028eb4512c2f1cf2a28c8ba8a2e5420195
KanashiQba <94380630+KanashiQba@users.noreply.github.com>
2022-06-29 12:41:34 +0200

    contact

---
+++ Project/Contact.php
@@ -0,0 +1,8 @@
+<?php include "PHPfiles/connection.php" ?>
+<?php include "PHPfiles/HeadFoot/header.php" ?>
+  <main>
+    <div class="break">           
+    <div class="break2">
+ 
+    </main>
+    <?php include "PHPfiles/HeadFoot/footer.php" ?>
