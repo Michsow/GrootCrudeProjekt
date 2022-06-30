429f282eb951a9fc01d132918b33d010623eb77f
KanashiQba <94380630+KanashiQba@users.noreply.github.com>
2022-06-30 13:46:03 +0200

    contact

--- Project/PHPfiles/Form.php
+++ Project/PHPfiles/Form.php
@@ -10,23 +10,19 @@
     </div>
 
     <div class="container">
-      <label for="uname"><b class="text2137">Username</b></label>
+
       <input type="text" placeholder="Enter Username" name="UserName" id="UserName" >
       
-      <label for="psw"><b class="text2137">Password</b></label>
+
       <input type="password" placeholder="Enter Password" name="UserPassword" id="UserPassword" >
         
       <button type="submit">Login</button>
-      <label id="RM1">
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
     </div>
 
     <div class="container" style="background-color:#f1f1f1">
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
 
-      <span class="UserPassword">Forgot <a href="#">password?</a></span>
     </div>
-    <p>  <a class="Registerbutton" href="PHPfiles/Register.php" style="color:black;">No account? Here you can make one.</a>  </p>
+    <p>  <a  href="PHPfiles/Register.php" style="color:black;" class="text">No account? Here you can make one.</a>  </p>
   </form>
 </div>
--- Project/PHPfiles/HeadFoot/footer.php
+++ Project/PHPfiles/HeadFoot/footer.php
@@ -4,6 +4,7 @@
     <script src="js/main.js"> </script>
     <script scr="js/JavaForm.js"></script>
     <script src="js/image.js"> </script>
+    <script src="js/contact.js"> </script>
 </footer>
 </body>
 </html>
